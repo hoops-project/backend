5826052b89683a71ccb94ea446e00c51b502a532
ramyo564 <yohan032yohan@gmail.com>
2024-05-06 00:25:50 +0900

    refactor : 테스트코드 수정

--- src/test/java/com/zerobase/hoops/gameUsers/controller/GameUserControllerTest.java
+++ src/test/java/com/zerobase/hoops/gameUsers/controller/GameUserControllerTest.java
@@ -1,20 +1,38 @@
 package com.zerobase.hoops.gameUsers.controller;
 
+import static com.zerobase.hoops.gameCreator.type.CityName.SEOUL;
+import static com.zerobase.hoops.gameCreator.type.FieldStatus.INDOOR;
+import static com.zerobase.hoops.gameCreator.type.Gender.ALL;
+import static com.zerobase.hoops.gameCreator.type.MatchFormat.THREEONTHREE;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.zerobase.hoops.entity.GameEntity;
 import com.zerobase.hoops.gameCreator.type.CityName;
+import com.zerobase.hoops.gameCreator.type.FieldStatus;
+import com.zerobase.hoops.gameCreator.type.Gender;
+import com.zerobase.hoops.gameCreator.type.MatchFormat;
 import com.zerobase.hoops.gameUsers.dto.GameSearchResponse;
 import com.zerobase.hoops.gameUsers.service.GameUserService;
 import com.zerobase.hoops.security.TokenProvider;
 import com.zerobase.hoops.users.service.UserService;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -38,49 +56,109 @@ class GameUserControllerTest {
   @MockBean
   private TokenProvider tokenProvider;
 
+  @DisplayName("필터 검색 테스트")
   @WithMockUser
   @Test
-  void searchMain_shouldReturnAllUpcomingGames() throws Exception {
+  public void testFindFilteredGames() throws Exception {
     // Given
-    List<GameSearchResponse> upcomingGames = Arrays.asList(
-        GameSearchResponse.builder().gameId(1L).build(),
-        GameSearchResponse.builder().gameId(2L).build()
-    );
-    when(gameUserService.searchMain()).thenReturn(upcomingGames);
+    LocalDate localDate = LocalDate.now();
+    CityName cityName = SEOUL;
+    FieldStatus fieldStatus = INDOOR;
+    Gender gender = ALL;
+    MatchFormat matchFormat = THREEONTHREE;
+    List<GameSearchResponse> gameSearchResponses = Arrays.asList(
+        GameSearchResponse.of(mock(GameEntity.class)));
 
-    // When, Then
-    mockMvc.perform(get("/api/game-user/search").with(csrf()))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.length()").value(2))
-        .andExpect(jsonPath("$[0].gameId").value(1L))
-        .andExpect(jsonPath("$[1].gameId").value(2L))
-        .andExpect(header().doesNotExist("Location"));
-  }
+    // When
+    when(gameUserService.findFilteredGames(any(), any(), any(),
+        any(), any())).thenReturn(gameSearchResponses);
 
-  @WithMockUser
-  @Test
-  void searchCity_shouldReturnUpcomingGamesForGivenCity()
-      throws Exception {
-    // Given
-    CityName cityName = CityName.SEOUL;
-    List<GameSearchResponse> upcomingGames = Arrays.asList(
-        GameSearchResponse.builder().gameId(1L).cityName(cityName).build(),
-        GameSearchResponse.builder().gameId(2L).cityName(cityName).build()
-    );
-    when(gameUserService.searchCity(cityName)).thenReturn(upcomingGames);
-
-    // When, Then
-    mockMvc.perform(get("/api/game-user/search-city")
-            .param("cityName", cityName.name())
+    // Then
+    mockMvc.perform(get("/api/game-user/search")
+            .param("localDate", localDate.toString())
+            .param("cityName", cityName.toString())
+            .param("fieldStatus", fieldStatus.toString())
+            .param("gender", gender.toString())
+            .param("matchFormat", matchFormat.toString())
             .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$.length()").value(2))
-        .andExpect(jsonPath("$[0].gameId").value(1L))
-        .andExpect(jsonPath("$[0].cityName").value(cityName.name()))
-        .andExpect(jsonPath("$[1].gameId").value(2L))
-        .andExpect(jsonPath("$[1].cityName").value(cityName.name()));
+        .andDo(print())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$").isArray());
   }
 
+  @DisplayName("필터 검색 데이터 테스트")
+  @WithMockUser
+  @Test
+  public void findFilteredGames_withFilters_shouldReturnFilteredGames()
+      throws Exception {
+    // Given
+    LocalDate localDate = LocalDate.now();
+    CityName cityName = CityName.SEOUL;
+    FieldStatus fieldStatus = FieldStatus.INDOOR;
+    Gender gender = Gender.ALL;
+    MatchFormat matchFormat = MatchFormat.THREEONTHREE;
+
+    LocalDateTime time = LocalDateTime.of(2024, 5, 6, 23, 54, 32, 8229099);
+    GameEntity gameEntity = new GameEntity();
+    gameEntity.setGameId(1L);
+    gameEntity.setTitle("Test Game");
+    gameEntity.setContent("Test Game Content");
+    gameEntity.setHeadCount(6L);
+    gameEntity.setFieldStatus(fieldStatus);
+    gameEntity.setGender(gender);
+    gameEntity.setStartDateTime(time.plusDays(1));
+    gameEntity.setCreatedDateTime(time);
+    gameEntity.setDeletedDateTime(null);
+    gameEntity.setInviteYn(true);
+    gameEntity.setAddress("Test Address");
+    gameEntity.setLatitude(37.5665);
+    gameEntity.setLongitude(126.9780);
+    gameEntity.setCityName(cityName);
+    gameEntity.setMatchFormat(matchFormat);
+    List<GameSearchResponse> expectedGames = Arrays.asList(
+        GameSearchResponse.of(gameEntity));
+
+    // When
+    when(gameUserService.findFilteredGames(eq(localDate), eq(cityName),
+        eq(fieldStatus), eq(gender), eq(matchFormat)))
+        .thenReturn(expectedGames);
+
+    // Then
+    mockMvc.perform(get("/api/game-user/search")
+            .param("localDate", localDate.toString())
+            .param("cityName", cityName.name())
+            .param("fieldStatus", fieldStatus.name())
+            .param("gender", gender.name())
+            .param("matchFormat", matchFormat.name())
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.size()").value(expectedGames.size()))
+        .andExpect(jsonPath("$[0].gameId").value(gameEntity.getGameId()))
+        .andExpect(jsonPath("$[0].title").value(gameEntity.getTitle()))
+        .andExpect(jsonPath("$[0].content").value(gameEntity.getContent()))
+        .andExpect(
+            jsonPath("$[0].headCount").value(gameEntity.getHeadCount()))
+        .andExpect(jsonPath("$[0].fieldStatus").value(
+            gameEntity.getFieldStatus().name()))
+        .andExpect(
+            jsonPath("$[0].gender").value(gameEntity.getGender().name()))
+        .andExpect(jsonPath("$[0].startDateTime").value(
+            gameEntity.getStartDateTime().toString()))
+        .andExpect(
+            jsonPath("$[0].inviteYn").value(gameEntity.getInviteYn()))
+        .andExpect(jsonPath("$[0].address").value(gameEntity.getAddress()))
+        .andExpect(
+            jsonPath("$[0].latitude").value(gameEntity.getLatitude()))
+        .andExpect(
+            jsonPath("$[0].longitude").value(gameEntity.getLongitude()))
+        .andExpect(jsonPath("$[0].cityName").value(
+            gameEntity.getCityName().name()))
+        .andExpect(jsonPath("$[0].matchFormat").value(
+            gameEntity.getMatchFormat().name()));
+  }
+
+  @DisplayName("주소 검색 테스트")
   @WithMockUser
   @Test
   void searchAddress_shouldReturnUpcomingGamesForGivenAddress()
--- src/test/java/com/zerobase/hoops/gameUsers/service/GameUserServiceTest.java
+++ src/test/java/com/zerobase/hoops/gameUsers/service/GameUserServiceTest.java
@@ -7,12 +7,17 @@ import static org.mockito.Mockito.when;
 
 import com.zerobase.hoops.entity.GameEntity;
 import com.zerobase.hoops.gameCreator.type.CityName;
+import com.zerobase.hoops.gameCreator.type.FieldStatus;
+import com.zerobase.hoops.gameCreator.type.Gender;
+import com.zerobase.hoops.gameCreator.type.MatchFormat;
 import com.zerobase.hoops.gameUsers.dto.GameSearchResponse;
 import com.zerobase.hoops.gameUsers.repository.GameUserRepository;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -20,6 +25,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import org.springframework.data.jpa.domain.Specification;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -31,50 +37,41 @@ class GameUserServiceTest {
   @InjectMocks
   private GameUserService gameUserService;
 
+  @DisplayName("GameUserService 필터 테스트 1")
   @Test
-  void searchMain_shouldReturnAllUpcomingGames() {
-    // Given
-    List<GameEntity> upcomingGames = Arrays.asList(
-        GameEntity.builder().gameId(1L)
-            .startDateTime(LocalDateTime.now().plusHours(1)).build(),
-        GameEntity.builder().gameId(2L)
-            .startDateTime(LocalDateTime.now().plusDays(2)).build()
-    );
-    when(gameUserRepository.findAllFromDateToday()).thenReturn(
-        upcomingGames);
+  void findFilteredGames_whenAllFiltersAreNull_shouldReturnAllGames() {
+    List<GameEntity> gameEntities = Arrays.asList(new GameEntity(),
+        new GameEntity());
+    when(gameUserRepository.findAll(any(Specification.class))).thenReturn(
+        gameEntities);
 
-    // When
-    List<GameSearchResponse> result = gameUserService.searchMain();
+    List<GameSearchResponse> result = gameUserService.findFilteredGames(null,
+        null, null, null, null);
 
-    // Then
-    assertEquals(2, result.size());
-    assertEquals(1L, result.get(0).getGameId());
-    assertEquals(2L, result.get(1).getGameId());
+    assertEquals(gameEntities.size(), result.size());
   }
 
+  @DisplayName("GameUserService 필터 테스트 2")
   @Test
-  void searchCity_shouldReturnUpcomingGamesForGivenCity() {
-    // Given
+  void findFilteredGames_whenSomeFiltersAreProvided_shouldReturnFilteredGames() {
+    LocalDate date = LocalDate.now();
     CityName cityName = CityName.SEOUL;
-    LocalDateTime now = LocalDateTime.now();
-    List<GameEntity> upcomingGames = Arrays.asList(
-        GameEntity.builder().gameId(1L).cityName(cityName)
-            .startDateTime(now.plusDays(1)).build(),
-        GameEntity.builder().gameId(2L).cityName(cityName)
-            .startDateTime(now.plusDays(7)).build()
-    );
-    when(gameUserRepository.findByCityNameAndStartDateTimeAfterOrderByStartDateTimeAsc(eq(cityName), any(LocalDateTime.class)))
-        .thenReturn(upcomingGames);
+    FieldStatus fieldStatus = FieldStatus.INDOOR;
+    Gender gender = Gender.ALL;
+    MatchFormat matchFormat = MatchFormat.FIVEONFIVE;
 
-    // When
-    List<GameSearchResponse> result = gameUserService.searchCity(cityName);
+    List<GameEntity> gameEntities = Arrays.asList(new GameEntity(),
+        new GameEntity());
+    when(gameUserRepository.findAll(any(Specification.class))).thenReturn(
+        gameEntities);
 
-    // Then
-    assertEquals(2, result.size());
-    assertEquals(1L, result.get(0).getGameId());
-    assertEquals(2L, result.get(1).getGameId());
+    List<GameSearchResponse> result = gameUserService.findFilteredGames(date,
+        cityName, fieldStatus, gender, matchFormat);
+
+    assertEquals(gameEntities.size(), result.size());
   }
 
+  @DisplayName("GameUserService 주소 찾기 테스트")
   @Test
   void searchAddress_shouldReturnUpcomingGamesForGivenAddress() {
     // Given
@@ -86,8 +83,9 @@ class GameUserServiceTest {
             .startDateTime(LocalDateTime.now().plusDays(2)).build()
     );
     when(
-        gameUserRepository.findByAddressContainingAndStartDateTimeAfterOrderByStartDateTimeAsc(
-            eq(address), any(LocalDateTime.class))).thenReturn(upcomingGames);
+        gameUserRepository.findByAddressContainingIgnoreCaseAndStartDateTimeAfterOrderByStartDateTimeAsc(
+            eq(address), any(LocalDateTime.class))).thenReturn(
+        upcomingGames);
 
     // When
     List<GameSearchResponse> result = gameUserService.searchAddress(
