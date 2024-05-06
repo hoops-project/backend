b2e6a206b78719e7f428240b235edf7fefa54744
ramyo564 <yohan032yohan@gmail.com>
2024-05-07 00:14:22 +0900

    style : 공백 및 주석 정리

--- src/test/java/com/zerobase/hoops/reports/controller/ReportControllerTest.java
+++ src/test/java/com/zerobase/hoops/reports/controller/ReportControllerTest.java
@@ -53,17 +53,17 @@ class ReportControllerTest {
   @Test
   @DisplayName("신고 성공")
   void report_success() throws Exception {
-    //given
+    // Given
     given(userRepository.existsByEmail(anyString()))
         .willReturn(true);
 
-    //when
+    // When
     ReportDto reportDto = ReportDto.builder()
         .reportedUserEmail("reported@example.com")
         .content("ReasonReasonReasonReasonReasonReasonReasonReasonReason")
         .build();
 
-    // 결과 확인
+    // Then
     ResultActions resultActions =
         mockMvc.perform(
                 post("/api/report/user")
