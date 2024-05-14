b3c6f860b4a9a58b6f50e5c95d2a6feddb08c702
Leejaeeun59357 <ne222727@gmail.com>
2024-05-15 08:10:12 +0900

    [feat] 알림 기능을 위한 Entity, Dto 생성

---
+++ src/main/java/com/zerobase/hoops/alarm/domain/NotificationEntity.java
@@ -0,0 +1,35 @@
+package com.zerobase.hoops.alarm.domain;
+
+import com.zerobase.hoops.entity.UserEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class NotificationEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(nullable = false)
+  private UserEntity receiver;
+
+  private String content;
+  private LocalDateTime createdDateTime;
+
+}
