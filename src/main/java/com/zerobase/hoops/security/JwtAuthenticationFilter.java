01f5d8bafd5cbd5ed14f6f9541bc490016e0aae8
Leejaeeun59357 <ne222727@gmail.com>
2024-05-25 16:10:29 +0900

    [fix] 웹소켓 엔드포인트로 접근 시 403 에러 발생 해결
    - 웹소켓 엔드포인트 인증과정 없이 통과하도록 수정
    - 불필요한 어노테이션 삭제

--- src/main/java/com/zerobase/hoops/security/JwtAuthenticationFilter.java
+++ src/main/java/com/zerobase/hoops/security/JwtAuthenticationFilter.java
@@ -71,7 +71,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
   private boolean isPublicPath(String requestURI) {
     List<String> publicPaths = List.of(
         "/api/user", "/api/auth/login", "/api/oauth2/login/kakao",
-    "/swagger-ui", "/v3/api-docs", "/api/game-user", "/ws");
+    "/swagger-ui", "/v3/api-docs", "/api/game-user", "/chat", "/pub", "/sub");
     return publicPaths.stream().anyMatch(requestURI::startsWith);
   }
 }
