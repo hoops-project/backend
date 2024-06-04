77a89e05d806823fc903ac3dcccc63411b788f8d
hj <kwonfelix93@gmail.com>
2024-06-04 12:24:36 +0900

    refactor : user 테이블 컬럼명 수정에 따른 테스트 코드, 변수 명 및 서비스 명 수정

--- src/main/java/com/zerobase/hoops/security/TokenProvider.java
+++ src/main/java/com/zerobase/hoops/security/TokenProvider.java
@@ -49,36 +49,37 @@ public class TokenProvider {
   /**
    * AccessToken 생성
    */
-  public String createAccessToken(String id, String email, List<String> role) {
-    return generateAccessToken(id, email, role, ACCESS_TOKEN_EXPIRE_TIME);
+  public String createAccessToken(String LoginId, String email,
+      List<String> role) {
+    return generateAccessToken(LoginId, email, role, ACCESS_TOKEN_EXPIRE_TIME);
   }
 
   /**
    * RefreshToken 생성
    */
-  public String createRefreshToken(String id) {
+  public String createRefreshToken(String LoginId) {
 
     String refreshToken =
-        generateRefreshToken(id, REFRESH_TOKEN_EXPIRE_TIME);
+        generateRefreshToken(LoginId, REFRESH_TOKEN_EXPIRE_TIME);
 
     authRepository.saveRefreshToken(
-        id, refreshToken, Duration.ofMillis(REFRESH_TOKEN_EXPIRE_TIME));
+        LoginId, refreshToken, Duration.ofMillis(REFRESH_TOKEN_EXPIRE_TIME));
     return refreshToken;
   }
 
-  public String generateAccessToken(String id, String email,
+  public String generateAccessToken(String LoginId, String email,
       List<String> roles, Long expireTime) {
 
-    Claims claims = Jwts.claims().setSubject(id);
+    Claims claims = Jwts.claims().setSubject(LoginId);
     claims.put("email", email);
     claims.put("roles", roles);
 
     return returnToken(claims, expireTime);
   }
 
-  public String generateRefreshToken(String id, Long expireTime) {
+  public String generateRefreshToken(String LoginId, Long expireTime) {
 
-    Claims claims = Jwts.claims().setSubject(id);
+    Claims claims = Jwts.claims().setSubject(LoginId);
 
     return returnToken(claims, expireTime);
   }
