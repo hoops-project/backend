b9df5ce09d969cf67a2e14f4c3e4f88f60c15149
ramyo564 <yohan032yohan@gmail.com>
2024-06-27 01:54:02 +0900

    refactor : 컨트롤러 ApiResponse -> ApiResponseFactory 로 변경 및 리팩토링
    1. 스웨거 스키마 추가
    2. ApiResponse -> ApiResponseFactory 주입 및 형식 통일

--- src/main/java/com/zerobase/hoops/reports/controller/ReportController.java
+++ src/main/java/com/zerobase/hoops/reports/controller/ReportController.java
@@ -1,14 +1,23 @@
 package com.zerobase.hoops.reports.controller;
 
-import com.zerobase.hoops.commonResponse.ApiResponse;
+import com.zerobase.hoops.commonResponse.ApiResponseFactory;
+import com.zerobase.hoops.commonResponse.BasicApiResponse;
+import com.zerobase.hoops.commonResponse.swaggerSchema.ErrorResponse;
+import com.zerobase.hoops.commonResponse.swaggerSchema.ReportResponse;
+import com.zerobase.hoops.commonResponse.swaggerSchema.ReportResponse.ReportContent;
 import com.zerobase.hoops.reports.dto.ReportDto;
 import com.zerobase.hoops.reports.dto.ReportListResponseDto;
 import com.zerobase.hoops.reports.service.ReportService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -26,36 +35,72 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/report")
 @RequiredArgsConstructor
-@Tag(name = "4. REPORT")
+@Tag(name = "REPORT")
 public class ReportController {
 
   private final ReportService reportService;
+  private final ApiResponseFactory apiResponseFactory;
 
+  @Operation(summary = "유저간 신고 기능")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "유저 신고 기능 성공",
+          content = @Content(schema = @Schema(implementation = ReportResponse.ReportUser.class))),
+      @ApiResponse(responseCode = "400", description = "커스텀 에러",
+          content = @Content(schema = @Schema(implementation = ErrorResponse.CustomError.class))),
+      @ApiResponse(responseCode = "403", description = "리프레시 토큰 만료",
+          content = @Content(schema = @Schema(implementation = ErrorResponse.ExpiredRefreshToken.class))),
+      @ApiResponse(responseCode = "500", description = "서버 에러 표시",
+          content = @Content(schema = @Schema(implementation = ErrorResponse.ServerError.class)))
+  }
+  )
   @PreAuthorize("hasRole('USER')")
   @PostMapping("/user")
-  public ResponseEntity<ApiResponse> report(
+  public ResponseEntity<BasicApiResponse> report(
       @RequestBody @Valid ReportDto request) {
     this.reportService.reportUser(request);
     return ResponseEntity.ok().body(
-        new ApiResponse("유저신고", "Success"));
+        apiResponseFactory.createSuccessResponse("유저신고"));
   }
 
+  @Operation(summary = "신고된 유저 리스트")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "신고된 리스트 출력",
+          content = @Content(schema = @Schema(implementation = ReportResponse.PageReportUsersList.class))),
+      @ApiResponse(responseCode = "400", description = "커스텀 에러",
+          content = @Content(schema = @Schema(implementation = ErrorResponse.CustomError.class))),
+      @ApiResponse(responseCode = "403", description = "리프레시 토큰 만료",
+          content = @Content(schema = @Schema(implementation = ErrorResponse.ExpiredRefreshToken.class))),
+      @ApiResponse(responseCode = "500", description = "서버 에러 표시",
+          content = @Content(schema = @Schema(implementation = ErrorResponse.ServerError.class)))
+  })
   @PreAuthorize("hasRole('OWNER')")
   @GetMapping("/user-list")
   public ResponseEntity<Page<ReportListResponseDto>> reportList(
-      @RequestParam(value = "page",defaultValue = "0") @Positive int page,
-      @RequestParam(value = "size",defaultValue = "10") @Positive int size) {
+      @RequestParam(value = "page", defaultValue = "0") @Positive int page,
+      @RequestParam(value = "size", defaultValue = "10") @Positive int size) {
     return ResponseEntity.ok()
         .body(this.reportService.reportList(page, size));
   }
 
+  @Operation(summary = "신고 내역 조회")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "신고 내역 조회",
+          content = @Content(schema = @Schema(implementation = ReportContent.class))),
+      @ApiResponse(responseCode = "400", description = "커스텀 에러",
+          content = @Content(schema = @Schema(implementation = ErrorResponse.CustomError.class))),
+      @ApiResponse(responseCode = "403", description = "리프레시 토큰 만료",
+          content = @Content(schema = @Schema(implementation = ErrorResponse.ExpiredRefreshToken.class))),
+      @ApiResponse(responseCode = "500", description = "서버 에러 표시",
+          content = @Content(schema = @Schema(implementation = ErrorResponse.ServerError.class)))
+  })
   @PreAuthorize("hasRole('OWNER')")
   @GetMapping("/contents/{report_id}")
-  public ResponseEntity<ApiResponse> reportContents(
+  public ResponseEntity<BasicApiResponse> reportContents(
+      @Parameter(description = "신고 유저 PK", name = "report_id")
       @PathVariable("report_id") @NotBlank String report_id) {
     return ResponseEntity.ok().body(
-        new ApiResponse("신고내역",
-            this.reportService.reportContents(report_id)));
+        apiResponseFactory.createSuccessWithDetailResponse(
+            "신고 내역", this.reportService.reportContents(report_id)));
   }
 
 
