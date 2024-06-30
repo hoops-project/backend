998471eb97fe03e866b879d03ca9eb1147318447
ramyo564 <yohan032yohan@gmail.com>
2024-06-30 19:29:10 +0900

    refactor : GameUser 컨트롤러 ApiResponse -> ApiResponseFactory 로 변경 및 리팩토링
    1. GameUserResponse 스웨거 스키마 추가
    2. ApiResponse -> ApiResponseFactory 주입 및 형식 통일
    3. 파라미터 추가
    4. Success Response 추가

--- src/main/java/com/zerobase/hoops/commonResponse/swaggerSchema/ErrorResponse.java
+++ src/main/java/com/zerobase/hoops/commonResponse/swaggerSchema/ErrorResponse.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 public class ErrorResponse {
 
+  // User Error
+
   @Schema(name = "ExpiredRefreshToken", description = "리프레시 토큰 만료 응답")
   public static class ExpiredRefreshToken extends
       com.zerobase.hoops.exception.ErrorResponse {
@@ -15,6 +17,20 @@ public class ErrorResponse {
     @Schema(description = "응답 상태", example = "403")
     private String statusCode;
   }
+
+  @Schema(name = "UserNotFound", description = "유저정보 없음")
+  public static class UserNotFound extends
+      com.zerobase.hoops.exception.ErrorResponse {
+
+    @Schema(description = "errorCode", example = "USER_NOT_FOUND")
+    private String errorCode;
+    @Schema(description = "errorMessage", example = "아이디가 존재하지 않습니다.")
+    private String errorMessage;
+    @Schema(description = "응답 상태", example = "400")
+    private String statusCode;
+  }
+
+  // Server Error
 
   @Schema(name = "ServerError", description = "서버에러")
   public static class ServerError extends
@@ -29,13 +45,28 @@ public class ErrorResponse {
 
   }
 
-  @Schema(name = "CustomError", description = "커스텀에러")
-  public static class CustomError extends
+  // Report Error
+
+  @Schema(name = "AlREADY_REPORTED", description = "이미 신고됨")
+  public static class AlreadyReported extends
       com.zerobase.hoops.exception.ErrorResponse {
 
-    @Schema(description = "errorCode", example = "CustomError")
+    @Schema(description = "errorCode", example = "AlREADY_REPORTED")
     private String errorCode;
-    @Schema(description = "errorMessage", example = "상황에 맞는 커스텀 에러.")
+    @Schema(description = "errorMessage", example = "이미 신고가 완료되었습니다.")
+    private String errorMessage;
+    @Schema(description = "응답 상태", example = "400")
+    private String statusCode;
+
+  }
+
+  @Schema(name = "NOT_EXIST_REPORTED", description = "신고 내용이 없음")
+  public static class NotExistReported extends
+      com.zerobase.hoops.exception.ErrorResponse {
+
+    @Schema(description = "errorCode", example = "NOT_EXIST_REPORTED")
+    private String errorCode;
+    @Schema(description = "errorMessage", example = "신고 내용이 없습니다..")
     private String errorMessage;
     @Schema(description = "응답 상태", example = "400")
     private String statusCode;
